1089e9b485749c127b1aa4240f7c34c465d0751c
Mohamed Sami <srmf2025@gmail.com>
2022-02-22 20:38:02 +0530

    add and change fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        body: ["DM Sans", "sans-serif"],
+        logo: ["Merriweather", "serif"],
+      },
+    },
   },
   plugins: [],
-}
+};
