5e33e3af3c7cde64d3d58cc1da724312b4b3a396
Mohamed Sami <srmf2025@gmail.com>
2022-02-23 20:03:10 +0530

    LatestArticles -> TrendingArticles

---
+++ components/TrendingArticles.tsx
@@ -0,0 +1,24 @@
+import ArticleCard from "./ArticleCard";
+
+const TrendingArticles = () => {
+  return (
+    <div className="">
+      <h1 className="text-xs font-bold">TRENDING RIGHT NOW</h1>
+      <div className="grid sm:grid-cols-2 lg:grid-cols-3">
+        {[...Array(6)].map((_, id) => (
+          <div className="flex flex-row items-start">
+            <div className="w-28 pt-3 text-center text-3xl font-bold text-gray-300 lg:pt-5">
+              0{id + 1}
+            </div>
+            <ArticleCard id={id} title="Lorem ipsum dolor sit">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+              consectetur lacus non dictum egestas.
+            </ArticleCard>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default TrendingArticles;
