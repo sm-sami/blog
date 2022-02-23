7e403cb90fe9ffc04e0ab9b05002ddc67cd46955
Mohamed Sami <srmf2025@gmail.com>
2022-02-23 20:29:19 +0530

    added index.ts to reexport components

---
+++ components/index.ts
@@ -0,0 +1,5 @@
+export { default as Layout } from "./Layout";
+export { default as Navbar } from "./Navbar";
+export { default as ArticleCard } from "./ArticleCard";
+export { default as TrendingArticles } from "./TrendingArticles";
+export { default as Footer } from "./Footer";
